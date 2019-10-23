975ccbdb4017bf1967cab7fcae5bec83e784dcfc
Jake Shams <jshams62@gmail.com>
2019-10-22 19:38:50 -0700

    implemented is_sorted, bubble sort, and selection sort

--- Code/sorting_iterative.py
+++ Code/sorting_iterative.py
@@ -5,16 +5,29 @@ def is_sorted(items):
     """Return a boolean indicating whether given items are in sorted order.
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Check that all adjacent items are in order, return early if so
+    for i in range(items - 1):
+        if items[i] > items[i + 1]:
+            return False
+    return True
 
 
 def bubble_sort(items):
     """Sort given items by swapping adjacent items that are out of order, and
     repeating until all items are in sorted order.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Repeat until all items are in sorted order
-    # TODO: Swap adjacent items that are out of order
+    Running time: O(n^2) because we have to iterate through the array n times.
+    Memory usage: 1, swaps happen in place, but flags are stored"""
+    is_sorted = False
+    iteration = 0
+    while not is_sorted or iteration == len(items) - 1:
+        had_swap = False
+        # iterate through the array in pairs and swap them if the latter is lesser
+        for i in range(len(items) - iteration - 1):
+            if items[i] > items[i + 1]:
+                items[i], items[i + 1] = items[i + 1], items[i]
+                had_swap = True
+        iteration += 1
+        if not had_swap:
+            is_sorted = True
 
 
 def selection_sort(items):
@@ -25,6 +38,9 @@ def selection_sort(items):
     # TODO: Repeat until all items are in sorted order
     # TODO: Find minimum item in unsorted items
     # TODO: Swap it with first unsorted item
+    for i in range(len(items)):
+        min_index = items.index(min(items[i:]))
+        items[i], items[min_index] = items[min_index], items[i]
 
 
 def insertion_sort(items):
