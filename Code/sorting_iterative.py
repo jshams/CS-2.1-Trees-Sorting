cb534b455e3700e8aa4b11bbdc7caa538e59f20d
Jake Shams <jshams62@gmail.com>
2019-10-22 20:21:42 -0700

    added comments and annotated time complexities

--- Code/sorting_iterative.py
+++ Code/sorting_iterative.py
@@ -3,11 +3,15 @@
 
 def is_sorted(items):
     """Return a boolean indicating whether given items are in sorted order.
-    TODO: Running time: O(n) because we have to go through the array once.
-    TODO: Memory usage: 0 because we only look at the array"""
+    Running time: O(n) because we have to go through the array once.
+    Memory usage: 0 because we only look at the array"""
+    # iterate through the items in the array in pairs
     for i in range(items - 1):
+        # check if the first item is greater than the latter
         if items[i] > items[i + 1]:
+            # if so return false
             return False
+    # if we've made it through the array with no items greater than their followers return true
     return True
 
 
@@ -16,53 +20,78 @@ def bubble_sort(items):
     repeating until all items are in sorted order.
     Running time: O(n^2) because we have to iterate through the array n times.
     Memory usage: 1, swaps happen in place, but flags are stored"""
+    # create a bool to indicate if the list is sorted
     is_sorted = False
+    # keep track of the iteration so we can ignore the already sorted items
     iteration = 0
+    # repeat bubble sort until items is sorted or when iterations is equal to the length of items - 1
     while not is_sorted or iteration == len(items) - 1:
+        # keep track of if there is a swap (if there's no swap items is sorted)
         had_swap = False
-        # iterate through the array in pairs and swap them if the latter is lesser
+        # iterate through the array in pairs
         for i in range(len(items) - iteration - 1):
+            # check if the former item is greater
             if items[i] > items[i + 1]:
+                # if so swap them
                 items[i], items[i + 1] = items[i + 1], items[i]
+                # set has_swap to True
                 had_swap = True
+        # increment iteration
         iteration += 1
+        # check if no swap happened
         if not had_swap:
+            # if so is_sorted is True so we can stop sorting
             is_sorted = True
 
 
 def selection_sort(items):
     """Sort given items by finding minimum item, swapping it with first
     unsorted item, and repeating until all items are in sorted order.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Repeat until all items are in sorted order
-    # TODO: Find minimum item in unsorted items
-    # TODO: Swap it with first unsorted item
+    Running time: O(n^2) always because we need to find the minimum 
+    element O(n) and swap it n times.
+    Memory usage: O(1) because swaps happen in place and only one
+    variable, i, is created"""
+
+    # iterate through i in the len of items to keep track of the length of the sorted part
     for i in range(len(items)):
+        # find the index of the minimum element
         min_index = items.index(min(items[i:]))
+        # swap the minimum element with the first unsorted item
         items[i], items[min_index] = items[min_index], items[i]
 
 
 def insertion_sort(items):
     """Sort given items by taking first unsorted item, inserting it in sorted
-    order in front of items, and repeating until all items are in order.`
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Repeat until all items are in sorted order
-    # TODO: Take first unsorted item
-    # TODO: Insert it in sorted order in front of items
+    order in front of items, and repeating until all items are in order.
+    Running time: O(n^2) because for each item (O(n)) we need to find its place 
+    in the sorted array (O(n)) and then insert it (O(n)). Really 2*O(n).
+    Memory usage: O(1) because we only keep track of the item, prev, and index."""
+
+    # keep track of the previous item
     prev = None
+    # iterate through each item in items keeping track of their index
     for i, item in enumerate(items):
-        if prev == None:
+        # check if prev is none
+        if prev is None:
+            # if so prev is item and continue
             prev = item
+        # otherwise
         else:
+            # check if the item is greater than the prev
             if item > prev:
+                # if so set prev to item and continue
                 prev = item
+            # otherwise we have to find its new place in the sorted part and insert it
             else:
-                # find its new place in the sorted part
+                # iterate through the sorted items in sorted part
                 for j in range(i):
+                    # if the sorted item is less than the item
                     if item < items[j]:
+                        # inser the item in its position
                         items.insert(j, item)
+                        # remove the item from its original position
                         items.pop(i + 1)
+                        # stop looking for a position
                         break
+                # set prev to the last sorted item
                 prev = items[i]
