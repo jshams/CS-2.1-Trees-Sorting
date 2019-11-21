b984cefb754161f4568582a96f865abecff2bcdb
Jake Shams <jshams62@gmail.com>
2019-11-20 18:10:25 -0800

    added update height and balance factor methods to avl node class

--- Code/avltree.py
+++ Code/avltree.py
@@ -26,6 +26,35 @@ class AVLNode(object):
     def has_right(self):
         return self.right is not None
 
+    def update_height(self):
+        if self.is_leaf():
+            self.height = 0
+        elif not self.has_left():
+            self.height = self.right.height + 1
+        elif not self.has_right():
+            self.height = self.left.height + 1
+        else:
+            if self.left.height > self.right.height:
+                self.height = self.left.height + 1
+            else:
+                self.height = self.right.height + 1
+
+    def balance_factor(self):
+        if self.is_leaf():
+            return 0
+        elif not self.has_right():
+            return -self.left.height - 1
+        elif not self.has_left():
+            return self.right.height + 1
+        else:
+            return self.right.height - self.left.height
+
+    def right_rotate(self):
+        pass
+
+    def left_rotate(self):
+        pass
+
 
 class AVLTree(object):
     def __init__(self, items=None):
