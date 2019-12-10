6dfb936e59f78ec68af4915f0519e9440652af00
Jake Shams <jshams62@gmail.com>
2019-12-09 16:02:18 -0800

    implemented counting dort

--- Code/sorting_integer.py
+++ Code/sorting_integer.py
@@ -6,11 +6,32 @@ def counting_sort(numbers):
     then looping over counts and copying that many numbers into output list.
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Find range of given numbers (minimum and maximum integer values)
-    # TODO: Create list of counts with a slot for each number in input range
-    # TODO: Loop over given numbers and increment each number's count
-    # TODO: Loop over counts and append that many numbers into output list
-    # FIXME: Improve this to mutate input instead of creating new output list
+    # Find range of given numbers (minimum and maximum integer values)
+    minimum = float('inf')
+    maximum = float('-inf')
+    for num in numbers:
+        if num > maximum:
+            maximum = num
+        if num < minimum:
+            minimum = num
+    # Create list of counts with a slot for each number in input range
+    num_places = [0] * (maximum - minimum + 1)
+    # Loop over given numbers and increment each number's count
+    for num in numbers:
+        num_places[num - minimum] += 1
+    # Loop over counts and append that many numbers into output list
+    # Improve this to mutate input instead of creating new output list
+    curr_index = 0
+    for num, count in enumerate(num_places):
+        for _ in range(count):
+            numbers[curr_index] = minimum + num
+            curr_index += 1    
+    return numbers
+
+nums = [3,5,2,6,1,1,6,2,3]
+sorted_nums = counting_sort(nums)
+print(sorted(nums))  
+    
 
 
 def bucket_sort(numbers, num_buckets=10):
